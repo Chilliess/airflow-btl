2f598a5f2269e7d55311468dcec791a57ec257f2
Chillies <23010526@st.phenikaa-uni.edu.com>
2026-06-08 09:17:11 +0700

    Add distributed worker monitoring

--- dags/dynamic_mapping.py
+++ dags/dynamic_mapping.py
@@ -1,6 +1,7 @@
 from airflow.decorators import dag, task
 from datetime import datetime
 import socket
+import time
 
 @dag(
     dag_id="dynamic_task_mapping",
@@ -19,15 +20,22 @@ def dynamic_task_mapping():
             "file_3.csv",
             "file_4.csv",
             "file_5.csv",
+            "file_6.csv",
+            "file_7.csv",
+            "file_8.csv",
+            "file_9.csv",
+            "file_10.csv",
         ]
 
     @task
     def process_file(filename: str):
         worker = socket.gethostname()
 
-        print(
-            f"{filename} được xử lý bởi {worker}"
-        )
+        print(f"START {filename} -> {worker}")
+
+        time.sleep(10)
+
+        print(f"END {filename} -> {worker}")
 
         return {
             "file": filename,
@@ -44,11 +52,7 @@ def dynamic_task_mapping():
             )
 
     files = get_files()
-
-    results = process_file.expand(
-        filename=files
-    )
-
+    results = process_file.expand(filename=files)
     summary(results)
 
 dynamic_task_mapping()
